377ca7752246c0813851b686ec8d9d190308e229
Peter Choo <pchoo@centiq.co.uk>
2016-05-26 14:55:35 +0100

    Improve configurability of the module

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This module was created to trigger an endpoint on our bespoke system which then queries the SirPortly database.  This has been created as a Module as the Macros run prior to the database transaction commit surrounding updates to tickets and ticket_updates.
 
-This module assumes your endpoint requires a GET parameter `auth` with a `user:key` value.
+The `MicroService URL` field is required.  If `Require an Endpoint?` is set to `Yes` any event endpoints left blank will not trigger.  If it is set to `No` then any event endpoints left blank will be fired to the `MicroService URL`.  This is useful if all events trigger the same URL.
 
 ## Installation
 
@@ -13,5 +13,8 @@ The instructions below assume you have access to modify the SirPortly installati
 3. Restart SirPortly with `restart sirportly`.
 4. Navigate to `Admin` > `Integration Options`.
 5. Check the checkbox next to `Aftercommit`.
-6. Fill in the URL, Authorisation User and Authorisation Key fields.
-7. Click `Save Configuration`
+6. Enter in the MicroService URL field (e.g. `http://api.example.com/`)
+7. Select `Yes` or `No` for `Require an endpoint?`
+8. Enter the endpoint for each event if required
+9. Enter any required `Query String` details (e.g. `auth=user:key`)
+10. Click `Save Configuration`
--- after_commit.rb
+++ after_commit.rb
@@ -6,39 +6,59 @@ module Sirportly
         description "Sends an HTTP request to an endpoint after a ticket is updated"
         author "Peter Choo <pchoo@centiq.co.uk>"
 
-        config_option :refresh_url, "MicroService Endpoint", "The URL of the refresh endpoint", :required => true
-        config_option :auth_user, "Authorisation User", "The User to authenticate as to the MicroService", :required => true
-        config_option :auth_key, "Authorisation Key", "The Authorisation Key for the MicroService", :required => true
+        public_description "Fires a request to an endpoint on Ticket and Ticket Update events.  If an endpoint is required and an event endpoint is left blank, that event will not trigger."
+
+        config_option :refresh_url, "MicroService URL", "The URL for the endpoints", :required => true
+
+        config_option :require_endpoint, "Require an Endpoint?", "If Yes, any event endpoints left blank will not trigger. If No, any endpoints left blank will call the base URL", :required => true, :options => ['Yes', 'No']
+
+        config_option :ticket_create_endpoint, "Ticket Create endpoint", "The endpoint to call when a Ticket is created"
+        config_option :ticket_update_endpoint, "Ticket Update endpoint", "The endpoint to call when a Ticket is updated"
+        config_option :ticket_destroy_endpoint, "Ticket Destroy endpoint", "The endpoint to call when a Ticket is destroyed"
+        config_option :ticket_update_create_endpoint, "Ticket Update Create endpoint", "The endpoint to call when a Ticket Update is created"
+        config_option :ticket_update_update_endpoint, "Ticket Update Update endpoint", "The endpoint to call when a Ticket Update is updated"
+        config_option :ticket_update_destroy_endpoint, "Ticket Update Destroy endpoint", "The endpoint to call when a Ticket Update is destroyed"
+
+        config_option :query_string, "Query String", "If supplied, the query string will be supplied to the URL"
 
         # Publicise makes the configuration options appear in the web interface for configuring
         publicise
 
+        add_callback :after, :ticket_update, :create do |config, ticket_update|
+          make_request(config, :ticket_update_create_endpoint, ticket_update.ticket)
+        end
+
         add_callback :after, :ticket_update, :update do |config, ticket_update|
-          make_request(config, ticket_update.ticket)
+          make_request(config, :ticket_update_update_endpoint, ticket_update.ticket)
+        end
+
+        add_callback :after, :ticket_update, :destroy do |config, ticket_update|
+          make_request(config, :ticket_update_destroy_endpoint, ticket_update.ticket)
         end
 
         add_callback :after, :ticket, :create do |config, ticket|
-          make_request(config, ticket)
+          make_request(config, :ticket_create_endpoint, ticket)
         end
 
         add_callback :after, :ticket, :update do |config, ticket|
-          make_request(config, ticket)
+          make_request(config, :ticket_update_endpoint, ticket)
         end
 
         add_callback :after, :ticket, :destroy do |config, ticket|
-          make_request(config, ticket)
+          make_request(config, :ticket_destroy_endpoint, ticket)
         end
 
         # Add self. to make this a class method
-        def self.check_config(config)
+        def self.check_config(config, event)
           uri = URI.parse(config[:refresh_url])
-          uri.is_a?(URI::HTTP) && !config[:auth_user].blank? && !config[:auth_key].blank?
+          uri.is_a?(URI::HTTP) && (!config[:require_endpoint] || !config[event].blank?)
         end
 
         # Add self. to make this a class method
-        def self.make_request(config, ticket)
-          if check_config(config)
-            uri = "#{config[:refresh_url]}?auth=#{config[:auth_user]}:#{config[:auth_key]}"
+        def self.make_request(config, event, ticket)
+          if check_config(config, event)
+            uri = "#{config[:refresh_url].sub(/\/$/, '')}/#{config[event]}"
+            uri << ( config[:query_string].blank? ? "" : "?#{config[:query_string]}" )
             ticket_payload = "{\"id\":#{ticket.id}}"
             Sirportly::HTTP.post(uri, {ticket: ticket_payload})
           end
@@ -47,4 +67,3 @@ module Sirportly
     end
   end
 end
-
